b707f3dacdd9b6c34da8c26aff6e46b7dc434c7b
SpeekeR <DominikZappe1999@seznam.cz>
2021-10-16 16:58:31 +0200

    DFA now accepts chars which are inputted from user via console

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         DFA dfa = new DFA();
-        System.out.println(dfa.accept("c"));
-        System.out.println(dfa.accept("aaaaaacb"));
-        System.out.println(dfa.accept("zzz"));
-        System.out.println(dfa.accept("aacbc"));
-        System.out.println();
-        System.out.println(dfa.accept('c'));
-        System.out.println(dfa.accept('b'));
-        System.out.println(dfa.accept('c'));
-        System.out.println(dfa.accept('q'));
-        dfa.restart();
-        System.out.println(dfa.accept('c'));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Start inputing symbols | Q (quit), R (restart)");
+        while(true) {
+            String input = sc.next();
+            char symbol = input.charAt(0);
+            if(symbol == 'Q' || symbol == 'q') {
+                System.out.println("Ending...");
+                break;
+            }
+            if(symbol == 'R' || symbol == 'r') {
+                dfa.restart();
+                System.out.println("DFA has been restarted.");
+                continue;
+            }
+            System.out.println("DFA is in accepting state: " + dfa.accept(symbol));
+        }
     }
 
 }
